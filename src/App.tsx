440c4a4be29d212086bac8d636361b71d1d7e5d2
Harsh Dabas <iamharshdabas@gmail.com>
2024-07-02 22:24:34 +0530

    chore: format

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Refine, WelcomePage } from "@refinedev/core";
+import { Refine, WelcomePage } from "@refinedev/core"
 
 function App() {
   return (
     <Refine>
       <WelcomePage />
     </Refine>
-  );
+  )
 }
 
-export default App;
+export default App
