d807ba518d8ac6a18101751ac889b44e31eacdce
Harsh Dabas <iamharshdabas@gmail.com>
2024-07-08 18:21:24 +0530

    feat: next steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { DevtoolsProvider, DevtoolsPanel } from "@refinedev/devtools"
 import CssBaseline from "@mui/material/CssBaseline"
 import GlobalStyles from "@mui/material/GlobalStyles"
 import { ThemeProvider } from "@mui/material/styles"
@@ -18,6 +19,7 @@ import ListProducts from "./pages/products/list"
 import ShowProduct from "./pages/products/show"
 import { authProvider } from "./providers/auth"
 import { dataProvider } from "./providers/data"
+import ListCategories from "./pages/categories/list"
 
 function App() {
   return (
@@ -27,65 +29,78 @@ function App() {
         <CssBaseline />
         <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
         <RefineSnackbarProvider>
-          <Refine
-            dataProvider={dataProvider}
-            authProvider={authProvider}
-            routerProvider={routerProvider}
-            notificationProvider={useNotificationProvider}
-            resources={[
-              {
-                name: "protected-products",
-                list: "/products",
-                show: "/products/:id",
-                edit: "/products/:id/edit",
-                create: "/products/create",
-                meta: { label: "Products" },
-              },
-            ]}
-          >
-            <Routes>
-              <Route
-                element={
-                  // We're wrapping our routes with the `<Authenticated />` component
-                  // We're omitting the `fallback` prop to redirect users to the login page if they are not authenticated.
-                  // If the user is authenticated, we'll render the `<Header />` component and the `<Outlet />` component to render the inner routes.
-                  <Authenticated key="authenticated-routes" redirectOnFail="/login">
-                    <ThemedLayoutV2
-                      Title={(props) => {
-                        return <ThemedTitleV2 {...props} text="Refine Tutorial" />
-                      }}
-                    >
-                      <Outlet />
-                    </ThemedLayoutV2>
-                  </Authenticated>
-                }
-              >
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              authProvider={authProvider}
+              routerProvider={routerProvider}
+              notificationProvider={useNotificationProvider}
+              resources={[
+                {
+                  name: "protected-products",
+                  list: "/products",
+                  show: "/products/:id",
+                  edit: "/products/:id/edit",
+                  create: "/products/create",
+                  meta: { label: "Products" },
+                },
+                // We're adding the categories resource to the resources array
+                // This way, there will be a link to the categories list in the sidebar
+                {
+                  name: "categories",
+                  list: "/categories",
+                  meta: { label: "Categories" },
+                },
+              ]}
+            >
+              <Routes>
                 <Route
-                  index
-                  // We're also replacing the <Navigate /> component with the <NavigateToResource /> component.
-                  // It's tailored version of the <Navigate /> component that will redirect to the resource's list route.
-                  element={<NavigateToResource resource="protected-products" />}
-                />
-                <Route path="/products">
-                  <Route index element={<ListProducts />} />
-                  <Route path=":id" element={<ShowProduct />} />
-                  <Route path=":id/edit" element={<EditProduct />} />
-                  <Route path="create" element={<CreateProduct />} />
+                  element={
+                    // We're wrapping our routes with the `<Authenticated />` component
+                    // We're omitting the `fallback` prop to redirect users to the login page if they are not authenticated.
+                    // If the user is authenticated, we'll render the `<Header />` component and the `<Outlet />` component to render the inner routes.
+                    <Authenticated key="authenticated-routes" redirectOnFail="/login">
+                      <ThemedLayoutV2
+                        Title={(props) => {
+                          return <ThemedTitleV2 {...props} text="Refine Tutorial" />
+                        }}
+                      >
+                        <Outlet />
+                      </ThemedLayoutV2>
+                    </Authenticated>
+                  }
+                >
+                  <Route
+                    index
+                    // We're also replacing the <Navigate /> component with the <NavigateToResource /> component.
+                    // It's tailored version of the <Navigate /> component that will redirect to the resource's list route.
+                    element={<NavigateToResource resource="protected-products" />}
+                  />
+                  <Route path="/products">
+                    <Route index element={<ListProducts />} />
+                    <Route path=":id" element={<ShowProduct />} />
+                    <Route path=":id/edit" element={<EditProduct />} />
+                    <Route path="create" element={<CreateProduct />} />
+                  </Route>
+                  <Route path="/categories">
+                    <Route index element={<ListCategories />} />
+                  </Route>
                 </Route>
-              </Route>
-              <Route
-                element={
-                  <Authenticated key="auth-pages" fallback={<Outlet />}>
-                    {/* We're also replacing the <Navigate /> component with the <NavigateToResource /> component. */}
-                    {/* It's tailored version of the <Navigate /> component that will redirect to the resource's list route. */}
-                    <NavigateToResource resource="protected-products" />
-                  </Authenticated>
-                }
-              >
-                <Route path="/login" element={<Login />} />
-              </Route>
-            </Routes>
-          </Refine>
+                <Route
+                  element={
+                    <Authenticated key="auth-pages" fallback={<Outlet />}>
+                      {/* We're also replacing the <Navigate /> component with the <NavigateToResource /> component. */}
+                      {/* It's tailored version of the <Navigate /> component that will redirect to the resource's list route. */}
+                      <NavigateToResource resource="protected-products" />
+                    </Authenticated>
+                  }
+                >
+                  <Route path="/login" element={<Login />} />
+                </Route>
+              </Routes>
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
         </RefineSnackbarProvider>
       </ThemeProvider>
     </BrowserRouter>
---
+++ src/pages/categories/list.tsx
@@ -0,0 +1,7 @@
+import { MuiInferencer } from "@refinedev/inferencer/mui"
+
+const ListCategories = () => {
+  return <MuiInferencer />
+}
+
+export default ListCategories
